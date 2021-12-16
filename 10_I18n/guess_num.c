3b71e813f7de4fa83a86ffe951f60e6cb9d77382
Alexandr Solovyov <26232688+vekle@users.noreply.github.com>
2021-12-16 23:47:49 +0300

    Homework 10_I18n

---
+++ 10_I18n/guess_num.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <string.h>
+
+#include <libintl.h>
+#include <locale.h>
+#define _(STRING) gettext(STRING)
+
+int main()
+{
+    setlocale(LC_ALL, "");
+    bindtextdomain("guess_num", ".");
+    textdomain("guess_num");
+
+    printf(_("Guess a number from 1 to 100\n"));
+
+    int answer_num = 0;
+    int left = 1, right = 100;
+    while (!answer_num) {
+        int mid = left + (right - left) / 2;
+
+        printf(_("Your number is greater than %d? yes/no "), mid);
+
+        char *user_answer = NULL;
+        size_t user_answer_len;
+        getline(&user_answer, &user_answer_len, stdin);
+        user_answer[strcspn(user_answer, "\n")] = 0;
+
+        if (strcmp(user_answer, _("yes")) == 0) {
+            left = mid + 1;
+        } else {
+            right = mid;
+        }
+
+        if (right <= left)
+            answer_num = right;
+    }
+
+    printf(_("You guessed number %d\n"), answer_num);
+}
